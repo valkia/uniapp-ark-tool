f72d25cb223ec7dbd1e73829683a6229a8faf2d5
dujiahao <610153956@qq.com>
2020-07-09 00:30:17 +0800

    角色

---
+++ pages/char/details.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<details-layout />
+	</view>
+</template>
+
+<script>
+	import DetailsLayout from '../../components/DetailsLayout'
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+		component: {DetailsLayout}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/char/list.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list:[]
+			}
+		},
+		methods: {
+			
+		},
+		component: {},
+		created(){
+			this.api.get2('/characterList',{}).then(data => {
+				this.list = data;
+			}).catch(function(msg) {
+				console.log(msg)
+			})
+		}
+	}
+</script>
+
+<style>
+
+</style>
